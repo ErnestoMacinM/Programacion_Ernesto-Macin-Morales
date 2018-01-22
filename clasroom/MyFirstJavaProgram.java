6bf40c196a672bcdfc6f301f36bd0ca4f845b4a8
ErnestoMacinM <linkinparkernesto@gmail.com>
2018-01-22 09:19:11 -0600

    Clasroom and homework folders ready

---
+++ clasroom/MyFirstJavaProgram.java
@@ -0,0 +1,18 @@
+
+/*********************
+*Ernesto Macin Morales
+*MyFirstJava.java
+*
+*
+*This program prints a "hello world" message
+***********************/
+public class MyFirstJavaProgram{
+
+public static void main(String[]args){
+  System.out.println("Hello world ");
+      System.out.println("cuantaos hombres quiere Diego:"+ args[0]);
+        System.out.println("posicion 1:"+ args[1]);
+          System.out.println("posicion 2:"+ args[2]);
+}
+
+}
