9d0a4c66c35cfd7586f435e217759433d5077d4d
ErnestoMacinM <linkinparkernesto@gmail.com>
2018-01-29 09:45:45 -0600

    JANUARY 29

--- clasroom/InputData.java
+++ clasroom/InputData.java
@@ -9,11 +9,11 @@ import java.util.Scanner;
 
 public class InputData{
   public static void main(String[]args){
-    System.out.println("Enter your name");
+  System.out.println("Enter your name");
   Scanner userData = new Scanner (System.in);
   String name;
-   name= userData.nextLine();
+  name= userData.nextLine();
 
-   System.out.println("Hello "+name);
+  System.out.println("Hello "+name);
  }// end main
 }//end InputData
